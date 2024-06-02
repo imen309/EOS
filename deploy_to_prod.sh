6230ed6cc19158de08436bfee0d308e052cd14cd
imen309 <imen.mettichi@esprit.tn>
2024-06-02 11:47:33 +0200

    edit files

---
+++ deploy_to_prod.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+TARGET_DIR="prod_manifests"
+
+if [ -d "$TARGET_DIR" ]; then
+  rm -rf "$TARGET_DIR"
+fi
+
+mkdir -p $TARGET_DIR/infrastructure
+mkdir -p $TARGET_DIR/microservices
+
+curl -o $TARGET_DIR/infrastructure/configmap.yml https://raw.githubusercontent.com/imen309/EOS/test/prod_manifests/infrastructure/configmap.yml
+curl -o $TARGET_DIR/infrastructure/elasticsearch-volumes.yml https://raw.githubusercontent.com/imen309/EOS/test/prod_manifests/infrastructure/elasticsearch-volumes.yml
+curl -o $TARGET_DIR/infrastructure/elasticsearch.yml https://raw.githubusercontent.com/imen309/EOS/test/prod_manifests/infrastructure/elasticsearch.yml
+curl -o $TARGET_DIR/infrastructure/postgres-volumes.yml https://raw.githubusercontent.com/imen309/EOS/test/prod_manifests/infrastructure/postgres-volumes.yml
+curl -o $TARGET_DIR/infrastructure/postgres.yml https://raw.githubusercontent.com/imen309/EOS/test/prod_manifests/infrastructure/postgres.yml
+
+curl -o $TARGET_DIR/microservices/ecomm-cart.yml https://raw.githubusercontent.com/imen309/EOS/test/prod_manifests/microservices/ecomm-cart.yml
+curl -o $TARGET_DIR/microservices/ecomm-order.yml https://raw.githubusercontent.com/imen309/EOS/test/prod_manifests/microservices/ecomm-order.yml
+curl -o $TARGET_DIR/microservices/ecomm-product.yml https://raw.githubusercontent.com/imen309/EOS/test/prod_manifests/microservices/ecomm-product.yml
+curl -o $TARGET_DIR/microservices/ecomm-front.yml https://raw.githubusercontent.com/imen309/EOS/test/prod_manifests/microservices/ecomm-front.yml
+curl -o $TARGET_DIR/microservices/ecomm-user.yml https://raw.githubusercontent.com/imen309/EOS/test/prod_manifests/microservices/ecomm-user.yml
+
+curl -o $TARGET_DIR/namespace.yml https://raw.githubusercontent.com/imen309/EOS/test/prod_manifests/namespace.yml
