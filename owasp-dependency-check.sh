e4e033bbf2465676379a5cf5a0e721d7588b7794
imen309 <imen.mettichi@esprit.tn>
2024-08-26 09:56:00 +0200

    updated

--- owasp-dependency-check.sh
+++ owasp-dependency-check.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# Définir les répertoires nécessaires
 OWASPDC_DIRECTORY=$HOME/OWASP-Dependency-Check
 DATA_DIRECTORY="$OWASPDC_DIRECTORY/data"
 REPORT_DIRECTORY="$OWASPDC_DIRECTORY/reports"
@@ -12,7 +13,10 @@ if [ ! -d "$DATA_DIRECTORY" ]; then
     chmod -R 777 "$REPORT_DIRECTORY"
 fi
 
-# Make sure we are using the latest version
+
+NVD_API_KEY="1676d6a0-7c25-4f36-91bd-27389b01e451"
+
+# Exécuter le scan avec Docker
 docker run --rm \
     --volume "`pwd`":/src \
     --volume "$DATA_DIRECTORY":/usr/share/dependency-check/data \
@@ -21,4 +25,5 @@ docker run --rm \
     --scan /src \
     --format "HTML" \
     --project "My OWASP Dependency Check Project" \
-    --out /report
+    --out /report \
+    --nvd-api-key "$NVD_API_KEY"
